0152185d01982235858960bddd7f88877c2c40d3
Moonsy8 <156733198+Moonsy8@users.noreply.github.com>
2024-10-21 14:39:26 +0800

    Ngefilm.kt

--- Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
+++ Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.mainPageOf
 
 class Ngefilm : Gomov() {
 
-    override var mainUrl = "https://ngefilm21.pics"
+    override var mainUrl = "https://new18.ngefilm21.yachts/"
 
     override var name = "Ngefilm"
     override val mainPage = mainPageOf(
